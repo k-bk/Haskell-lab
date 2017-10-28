de7551870f5fedb5224abf0e964e1d416a4e9885
karolBak <bak.karolek@gmail.com>
2017-10-28 10:09:59 +0200

    Finished chapter 'bout funcions from 'learn you a haskell'

--- lab1/ex8.hs
+++ lab1/ex8.hs
@@ -27,4 +27,20 @@ and' (a, b) =
 
 xor' :: (Bool, Bool) -> Bool
 xor' (a, b) = not' (a == b)
+
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+        | bmi <= 18.5 = "Under"
+        | bmi <= 25.0 = "Normal?"
+        | bmi <= 30.0 = "Fat"
+        | otherwise = "Ugh"
+        where bmi = bmiCalculate weight height
+
+bmiCalculate :: (RealFloat a) => a -> a -> a 
+bmiCalculate weight height = weight / height ^ 2
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+
+
            
